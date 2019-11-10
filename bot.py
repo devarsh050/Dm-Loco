f63765d9c5e54edc7d1eba41716248ae076eab1d
devarsh050 <55909448+devarsh050@users.noreply.github.com>
2019-11-10 14:48:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -276,7 +276,7 @@ while True:
     try:
         loop.run_until_complete(bot.start(BotToken, reconnect=True))
     except Exception as e:
-        time.sleep(1)
+        time.sleep(0.05)
         print("Event loop error:", e)
 
 
